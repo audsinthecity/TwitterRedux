2f72a072e02ceb367871332da6d673afb73ef716
audsinthecity <audrey.nyc@gmail.com>
2016-10-29 18:44:28 -0700

    API client midway

--- TwitterDemo/AppDelegate.swift
+++ TwitterDemo/AppDelegate.swift
@@ -47,34 +47,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let requestToken = BDBOAuth1Credential(queryString: url.query)
         
-        TwitterClient.sharedInstance?.fetchAccessToken(withPath: "oauth/access_token", method: "POST", requestToken: requestToken, success: { (accessToken: BDBOAuth1Credential?) -> Void in
+        let client = TwitterClient.sharedInstance
+        
+        client?.fetchAccessToken(withPath: "oauth/access_token", method: "POST", requestToken: requestToken, success: { (accessToken: BDBOAuth1Credential?) -> Void in
             print("I got the access token!")
             
-            TwitterClient.sharedInstance?.get("1.1/account/verify_credentials.json", parameters: nil, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
-                let userDictionary = response as! NSDictionary
-                
-                let user = User(dictionary: userDictionary)
-                //print("user: \(user)")
-                print("name: \(user.name)")
-                print("screenname: \(user.screenname)")
-                print("profile url: \(user.profileUrl)")
-                print("description: \(user.tagline)")
-                
-                }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
-                    print("error")
+            client?.homeTimeline(success: { (tweets: [Tweet]) -> () in
+                for tweet in tweets {
+                    print(tweet.text)
+                }
+                }, failure: { (error: Error) -> () in
+                print(error.localizedDescription)
             })
             
-            TwitterClient.sharedInstance?.get("1.1/statuses/home_timeline.json", parameters: nil, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
-                let dictionaries = response as! [NSDictionary]
-                
-                let tweets = Tweet.tweetsWithArray(dictionaries: dictionaries)
-                
-                for tweet in tweets {
-                    print("\(tweet.text!)")
-                }
-                }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
-                    print("Tweet error")
-            })
+            client?.currentAccount()
             
             }, failure: { (error: Error?) -> Void in
                 print("error: \(error?.localizedDescription)")
--- TwitterDemo/TwitterClient.swift
+++ TwitterDemo/TwitterClient.swift
@@ -12,5 +12,34 @@ import BDBOAuth1Manager
 class TwitterClient: BDBOAuth1SessionManager {
     
     static let sharedInstance = TwitterClient(baseURL: URL(string: "https://api.twitter.com")!, consumerKey: "xDbCXCHD0GaC1ZsFDa26yj2Ea", consumerSecret: "4V2jbGICckxg0YJrBQ6NHhjN9pH4EP1ZlUwdblhMNEipfTvuS4")
+    
+    func homeTimeline(success: @escaping ([Tweet]) -> (), failure: @escaping (Error) -> ()) {
+        get("1.1/statuses/home_timeline.json", parameters: nil, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
+            let dictionaries = response as! [NSDictionary]
+            
+            let tweets = Tweet.tweetsWithArray(dictionaries: dictionaries)
+            
+            success(tweets)
+        }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
+                failure(error)
+        })
+
+    }
+    
+    func currentAccount() {
+        get("1.1/account/verify_credentials.json", parameters: nil, progress: nil, success: { (task: URLSessionDataTask, response: Any?) -> Void in
+            let userDictionary = response as! NSDictionary
+            
+            let user = User(dictionary: userDictionary)
+            //print("user: \(user)")
+            print("name: \(user.name)")
+            print("screenname: \(user.screenname)")
+            print("profile url: \(user.profileUrl)")
+            print("description: \(user.tagline)")
+            
+            }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
+                print("error")
+        })
+    }
 
 }
